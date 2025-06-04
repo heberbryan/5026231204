d50a2c4a16db17c9c145377a1cd37c5be20f48e9
heberbryan <heber.bryan@gmail.com>
2025-06-04 09:53:30 +0700

    Pertemuan 14

--- app/Http/Controllers/PegawaiDBController.php
+++ app/Http/Controllers/PegawaiDBController.php
@@ -10,8 +10,9 @@ class PegawaiDBController extends Controller
     public function index()
     {
     	// mengambil data dari table pegawai
-        $pegawai = DB::table('pegawai')->get();
-
+        //$pegawai = DB::table('pegawai')->get(); //array all record
+        $pegawai = DB::table('pegawai')->paginate(10);
+        
     	// mengirim data pegawai ke view index
         return view('index',['pegawai' => $pegawai]);
 
@@ -37,13 +38,25 @@ class PegawaiDBController extends Controller
 	return redirect('/pegawai');
 
     }
+    // public function proses(Request $request)
+    // {
+    //     $this->validate($request,[
+    //        'nama' => 'required|min:5|max:20',
+    //        'pekerjaan' => 'required',
+    //        'usia' => 'required|numeric'
+    //     ]);
+ 
+    //     return view('proses',['data' => $request]);
+    // }
 
 
     // method untuk edit data pegawai
-    public function edit($id)
+    public function edit($id) //ada primary key
     {
 	// mengambil data pegawai berdasarkan id yang dipilih
-	$pegawai = DB::table('pegawai')->where('pegawai_id',$id)->get();
+	$pegawai = DB::table('pegawai')
+        ->where('pegawai_id',$id) //khusus operator =
+        ->get();
 	// passing data pegawai yang didapat ke view edit.blade.php
 	return view('edit',['pegawai' => $pegawai]);
     }
@@ -72,4 +85,20 @@ class PegawaiDBController extends Controller
 	    // alihkan halaman ke halaman pegawai
         return redirect('/pegawai');
     }
+
+    	public function cari(Request $request)
+	{
+		// menangkap data pencarian
+		$cari = $request->cari;
+ 
+    		// mengambil data dari table pegawai sesuai pencarian data
+		$pegawai = DB::table('pegawai')
+		->where('pegawai_nama','like',"%".$cari."%")
+		->paginate();
+ 
+    		// mengirim data pegawai ke view index
+		return view('index',['pegawai' => $pegawai]);
+ 
+	}
+
 }
